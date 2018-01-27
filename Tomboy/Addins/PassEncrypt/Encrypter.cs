2be29434b595d2344f47e8aba97554344c949eed
Piotr <contact.wisniowskipiotr@gmail.com>
2018-01-27 18:34:59 +0100

    A new PassEncryptAddin: Adds ability to Encrypt text. Just select text and press ctrl+e to encrypt some text. To decrypt it just click link and enter password. Decrypted text is shown and copied to clipboard.

---
+++ Tomboy/Addins/PassEncrypt/Encrypter.cs
@@ -0,0 +1,133 @@
+﻿//According to https://stackoverflow.com/questions/10168240/encrypting-decrypting-a-string-in-c-sharp CraigTP answer.
+using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+using System.Linq;
+
+namespace Tomboy.PassEncrypt
+{
+    public class Encrypter
+    {
+        /// <summary>
+        /// his constant is used to determine the keysize of the encryption algorithm in bits.
+        /// We divide this by 8 within the code below to get the equivalent number of bytes.
+        /// </summary>
+        protected const int Keysize = 256;
+
+        /// <summary>
+        /// This constant determines the number of iterations for the password bytes generation function.
+        /// </summary>
+        protected const int DerivationIterations = 1000;
+
+        /// <summary>
+        /// This method returns encrypted plainText by using Rijndael algoritm and user entered passPhrase.
+        /// Rijndael is more general than AES (see https://blogs.msdn.microsoft.com/shawnfa/2006/10/09/the-differences-between-rijndael-and-aes/)
+        /// </summary>
+        /// <param name="plainText"> Text to be encrypted. </param>
+        /// <param name="passPhrase"> Pasword which will be used for encrypting. </param>
+        /// <returns> Returns encrypted text. </returns>
+        public string Encrypt(string plainText, string passPhrase)
+        {
+            // Salt and IV is randomly generated each time, but is preprended to encrypted cipher text
+            // so that the same Salt and IV values can be used when decrypting.  
+            var saltStringBytes = Generate256BitsOfRandomEntropy();
+            var ivStringBytes = Generate256BitsOfRandomEntropy();
+            var plainTextBytes = Encoding.Unicode.GetBytes(plainText);
+            using (var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations))
+            {
+                var keyBytes = password.GetBytes(Keysize / 8);
+                using (var symmetricKey = new RijndaelManaged())
+                {
+                    symmetricKey.BlockSize = 256;
+                    symmetricKey.Mode = CipherMode.CBC;
+                    symmetricKey.Padding = PaddingMode.PKCS7;
+                    using (var encryptor = symmetricKey.CreateEncryptor(keyBytes, ivStringBytes))
+                    {
+                        using (var memoryStream = new MemoryStream())
+                        {
+                            using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                            {
+                                cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                                cryptoStream.FlushFinalBlock();
+                                // Create the final bytes as a concatenation of the random salt bytes, the random iv bytes and the cipher bytes.
+                                var cipherTextBytes = saltStringBytes;
+                                cipherTextBytes = cipherTextBytes.Concat(ivStringBytes).ToArray();
+                                cipherTextBytes = cipherTextBytes.Concat(memoryStream.ToArray()).ToArray();
+                                memoryStream.Close();
+                                cryptoStream.Close();
+                                return Convert.ToBase64String(cipherTextBytes);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method returns decrypted cipherText by using Rijndael algoritm and user entered passPhrase.
+        /// </summary>
+        /// <param name="cipherText"> Text to be decrypted. </param>
+        /// <param name="passPhrase"> Pasword which will be used for encrypting. </param>
+        /// <returns> Returns decrypted text. </returns>
+        public string Decrypt(string cipherText, string passPhrase)
+        {
+            try
+            {
+                // Get the complete stream of bytes that represent:
+                // [32 bytes of Salt] + [32 bytes of IV] + [n bytes of CipherText]
+                var cipherTextBytesWithSaltAndIv = Convert.FromBase64String(cipherText);
+                // Get the saltbytes by extracting the first 32 bytes from the supplied cipherText bytes.
+                var saltStringBytes = cipherTextBytesWithSaltAndIv.Take(Keysize / 8).ToArray();
+                // Get the IV bytes by extracting the next 32 bytes from the supplied cipherText bytes.
+                var ivStringBytes = cipherTextBytesWithSaltAndIv.Skip(Keysize / 8).Take(Keysize / 8).ToArray();
+                // Get the actual cipher text bytes by removing the first 64 bytes from the cipherText string.
+                var cipherTextBytes = cipherTextBytesWithSaltAndIv.Skip((Keysize / 8) * 2).Take(cipherTextBytesWithSaltAndIv.Length - ((Keysize / 8) * 2)).ToArray();
+
+                using (var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations))
+                {
+                    var keyBytes = password.GetBytes(Keysize / 8);
+                    using (var symmetricKey = new RijndaelManaged())
+                    {
+                        symmetricKey.BlockSize = 256;
+                        symmetricKey.Mode = CipherMode.CBC;
+                        symmetricKey.Padding = PaddingMode.PKCS7;
+                        using (var decryptor = symmetricKey.CreateDecryptor(keyBytes, ivStringBytes))
+                        {
+                            using (var memoryStream = new MemoryStream(cipherTextBytes))
+                            {
+                                using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                                {
+                                    var plainTextBytes = new byte[cipherTextBytes.Length];
+                                    var decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+                                    memoryStream.Close();
+                                    cryptoStream.Close();
+                                    return Encoding.Unicode.GetString(plainTextBytes, 0, decryptedByteCount);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (CryptographicException)
+            {
+                return Encrypt(cipherText, passPhrase);
+            }
+        }
+
+        /// <summary>
+        /// Generated random bytes.
+        /// </summary>
+        /// <returns> Returns random bytes. </returns>
+        protected byte[] Generate256BitsOfRandomEntropy()
+        {
+            var randomBytes = new byte[32]; // 32 Bytes will give us 256 bits.
+            using (var rngCsp = new RNGCryptoServiceProvider())
+            {
+                // Fill the array with cryptographically secure random bytes.
+                rngCsp.GetBytes(randomBytes);
+            }
+            return randomBytes;
+        }
+    }
+}
---
+++ Tomboy/Addins/PassEncrypt/PassEncryptTag.cs
@@ -0,0 +1,100 @@
+
+// (C) 2018 Piotr Wiśniowski <contact.wisniowskipiotr@gmail.com>, LGPL 2.1 or later. 
+using Gtk;
+using System;
+using System.Xml;
+using Tomboy;
+using System.IO;
+
+namespace Tomboy.PassEncrypt
+{
+	class PassEncryptTag : DynamicNoteTag
+	{
+        #region fields
+        /// <summary>
+        /// PassEncryptTag used in xml for writing tag to file.
+        /// </summary>
+        public static string TagName = "encpass";
+        /// <summary>
+        /// PassEncryptTag password atribute used in xml for writing tag to file.
+        /// </summary>
+        protected static string AtrName = "password";
+        #endregion
+
+        #region methods
+        /// <summary>
+        /// Initializes PassEncryptTag with element_name in xml.
+        /// </summary>
+        /// <param name="element_name"> Tag name which will be written in xml. </param>
+        public override void Initialize(string element_name)
+        {
+            base.Initialize(element_name);
+            this.Editable = false;
+            this.CanSplit = false;
+            this.CanSpellCheck = false;
+            this.CanGrow = false;
+            this.CanActivate = true;
+            this.Activated += PassEncryptTag_Activated;
+            this.SaveType = TagSaveType.Content;
+        }
+
+        /// <summary>
+        /// Sets password which will be saved in tag.
+        /// </summary>
+        /// <param name="encryptedPass"> Enctypred password which will be saved in tag. </param>
+        public void SetPassword(string encryptedPass)
+        {
+            this.Attributes.Add(AtrName, encryptedPass);
+        }
+        
+        /// <summary>
+        /// Method used when user clicks on tag. Synchronusly starts DecryptPassAsync().
+        /// </summary>
+        /// <param name="tag"> NoteTag which was clicked. </param>
+        /// <param name="editor"> NoteEditor in which NoteTag was clicked. </param>
+        /// <param name="start"> TextIter pointiong to beginning of tag. </param>
+        /// <param name="end"> TextIter pointiong to end of tag. </param>
+        /// <returns> Returns true if password contained in tag is not empty string. </returns>
+        protected bool PassEncryptTag_Activated(NoteTag tag, NoteEditor editor, Gtk.TextIter start, Gtk.TextIter end)
+        {
+            string encPass = (tag as PassEncryptTag).Attributes[AtrName];
+            if (string.IsNullOrWhiteSpace(encPass))
+                return false;
+            DecryptPassAsync(encPass);
+            return true;
+        }
+
+        /// <summary>
+        /// Method used do decrypt password. 
+        /// Shows new PasswordWindow and asks for decrypting passPhrase, then shows decrypted password on that window.
+        /// </summary>
+        /// <param name="encPass"> Encrypted password which needs to be decrypted by user entered passPhrase. </param>
+        protected async void DecryptPassAsync(string encPass)
+        {
+            PasswordWindow passwordWindow = new PasswordWindow(false);
+            passwordWindow.ShowAll();
+            string passPhrase = await passwordWindow.GetPassword();
+            if (string.IsNullOrEmpty(passPhrase))
+                return;
+
+            string decrypted = (new Encrypter()).Decrypt(encPass, passPhrase);
+            passwordWindow.ShowNonEditableText(decrypted);
+        }
+
+        /// <summary>
+        /// Saves PassEncryptTag in additional file near note just in case if tomboy deletes tag when addin is not found.
+        /// </summary>
+        public void SaveBackupCopy(string noteFilePath)
+        {
+            string path = noteFilePath + ".enctext";
+            int i = 0;
+            while( File.Exists(path) )
+            {
+                i++;
+                path = noteFilePath + ".enctext" + i.ToString();
+            }
+            File.WriteAllText(path,this.Attributes[AtrName]);
+        }
+        #endregion
+    }
+}
